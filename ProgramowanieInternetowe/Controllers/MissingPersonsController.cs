5e02aca7506cf597bbd96af60bcb854856fa5d9c
MarcinOsiadacz <marcin.osiadacz@outlook.com>
2021-02-28 13:28:32 +0100

    Added List View for MissingPersonModel.

---
+++ ProgramowanieInternetowe/Controllers/MissingPersonsController.cs
@@ -0,0 +1,95 @@
+﻿using ProgramowanieInternetowe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgramowanieInternetowe.Controllers
+{
+    public class MissingPersonsController : Controller
+    {
+        private readonly ProgramowanieInternetoweDbContext _db = ProgramowanieInternetoweDbContext.Create();
+
+        // GET: MissingPersons
+        public ActionResult Index()
+        {
+            return View(_db
+                .MissingPersons
+                .ToList()
+                );
+        }
+
+        // GET: MissingPersons/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MissingPersons/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //// POST: MissingPersons/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: MissingPersons/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: MissingPersons/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: MissingPersons/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: MissingPersons/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
